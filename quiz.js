ea0b4b0b6839d9f8b617e6d3416236fc4fe893fc
Bill Qubeka <Bill Qubeka>
2024-07-09 12:27:51 +0200

    add the functionality of quiz card

---
+++ quiz.js
@@ -0,0 +1,16 @@
+function checkAnswer() {
+    let correctAnswer = "4";
+
+    let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+    console.log(userAnswer);
+
+    if (userAnswer == correctAnswer) {
+        let feedback = document.getElementById('feedback');
+        feedback.textContent = "Correct! Well done.";
+    } else {
+        feedback.textContent = "That's incorrect. Try again!";
+    }
+}
+
+let submitButton = document.querySelector('#submit-answer');
+submitButton.addEventListener("click", checkAnswer);
